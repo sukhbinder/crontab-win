9bcb1363db610f522c0d1bdb8ce1dcbae1465258
aryeh-inc <saito-a@aryeh.co.jp>
2025-12-05 23:25:40 +0900

    fix and add features
    app.py
    
    fix: if I set the hour to 0 or set the minute to 0, parse_line() returns empty. 
         As a result Jobs such as 0:15 or 15:00 cannot be executed.
         According to "man 5 crontab", minutes range must be 0-59 and hours range must be 0-23.
    
    feat: Command line strings can contain spaces.
    
    feat: Names can also be used for the 'month' and 'day of week' fields.
          like "mon,wed,fri", "thu-tue", "jan-mar", "oct-feb".
    
    
    cli.py
    
    fix: "crontab show" fails if crontab file path contains spaces.
    
    feat: TEXT

--- crontab_win/app.py
+++ crontab_win/app.py
@@ -3,7 +3,7 @@ import time
 from datetime import datetime
 import subprocess
 from pathlib import Path
-import sys
+import sys, re
 
 event_schedule = sched.scheduler(time.time, time.sleep)
 
@@ -77,19 +77,71 @@ def clean_numbers(doms, mlist):
 
 
 def parse_line(line):
-    mins, hour, day, month, dow, *command = line.split()
-    minutes = list(range(1, 60))
-    hours = list(range(1, 24))
-    days = list(range(1, 32))
-    months = list(range(1, 13))
-    dows = list(range(0, 7))
+
+    # separate schedule-field and command-field
+    REGEX_SCHED=r'^(?P<sched>([0-9a-zA-Z\*\/\,\-]+\s*){5}|@[a-zA-Z]+\s*)'
+    match = re.search(REGEX_SCHED, line)
+    if match:
+        sched_str=match.group('sched').lower()
+    else:
+        # schedule-field missing
+        return
+
+    REGEX_CMD=r'^(?P<sched>([0-9a-zA-Z\*\/\,\-]+\s+){5}|@[a-zA-Z]+\s+)(?P<cmd>.+)'
+    match = re.search(REGEX_CMD, line)
+    if match:
+        cmd_str=match.group('cmd')
+    else:
+        cmd_str=""
+
+    #The name of the months/days into numbers
+    name_months = ["jan","feb","mar","apr","may","jun","jul","aug","sep","oct","nov","dec"]
+    name_days   = ["sun","mon","tue","wed","thu","fri","sat"]
+
+    if ("-sun" in sched_str):
+        sched_str = sched_str.replace("-sun","-sat,0",1)
+
+    # if name of the dow range spans the next week
+    REGEX_DOW_RANGE=f'(?P<left>.+)(?P<from>{"|".join(name_days)})' + f'-(?P<to>{"|".join(name_days)})(?P<right>.+)'
+    match = re.search(REGEX_DOW_RANGE,sched_str)
+    if match:
+        m_to   = name_days.index( match.group("to") )
+        m_from = name_days.index( match.group("from") )
+        if m_from > m_to :
+            sched_str = f'{match.group("left")}{match.group("from")}-sat,sun-{match.group("to")}{match.group("right")}'
+    
+    # if name of the month range spans the next year
+    REGEX_MON_RANGE=f'(?P<left>.+)(?P<from>{"|".join(name_months)})' + f'-(?P<to>{"|".join(name_months)})(?P<right>.+)'
+    match = re.search(REGEX_MON_RANGE,sched_str)
+    if match:
+        m_to   = name_months.index( match.group("to") )
+        m_from = name_months.index( match.group("from") )
+        if m_from > m_to :
+            sched_str = f'{match.group("left")}{match.group("from")}-dec,jan-{match.group("to")}{match.group("right")}'
+
+    # name of the dow to numbers
+    if any(d in sched_str for d in name_days):
+        for idx,d in enumerate(name_days):
+            sched_str = sched_str.replace(d,str(idx),2)
+
+    # name of the month to numbers
+    if any(m in sched_str for m in name_months):
+        for idx,m in enumerate(name_months):
+            sched_str = sched_str.replace(m,str(idx+1),2)
+
+    mins, hour, day, month, dow, *_rest = sched_str.split()
+    minutes = list(range(0, 60)) #0-59
+    hours = list(range(0, 24))   #0-23
+    days = list(range(1, 32))    #1-31
+    months = list(range(1, 13))  #1-12
+    dows = list(range(0, 7))     #0-6
 
     mins = clean_numbers(mins, minutes)
     hour = clean_numbers(hour, hours)
     day = clean_numbers(day, days)
     month = clean_numbers(month, months)
     dow = clean_numbers(dow, dows)
-    return mins, hour, day, month, dow, command
+    return mins, hour, day, month, dow, cmd_str
 
 
 def get_date():
@@ -102,7 +154,8 @@ def run_subprocess(cmdlist):
         si = subprocess.STARTUPINFO()
         si.dwFlags |= subprocess.STARTF_USESHOWWINDOW
     return subprocess.Popen(
-        cmdlist, startupinfo=si, stderr=subprocess.PIPE, stdout=subprocess.PIPE
+        cmdlist, startupinfo=si, stderr=subprocess.PIPE, stdout=subprocess.PIPE,
+        shell=True, text=True 
     )
 
 
